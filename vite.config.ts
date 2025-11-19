b9ed155cbceccb90bb390ced02b63a016557cfbb
maticerru <matiascerrudo18@gmail.com>
2025-11-19 18:03:57 -0300

    Corregir configuración base para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.NODE_ENV === 'production' ? '/sonrisa-moderna-main/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/sonrisa-moderna2/' : '/',
   server: {
     host: "::",
     port: 8080,
